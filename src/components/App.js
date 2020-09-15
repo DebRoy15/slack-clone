427c07a0ff7880125b2017ba064a90763c1055d6
DebRoy15 <debjyoti.roy1652@gmail.com>
2020-09-16 00:10:37 +0530

    added slack page with sidebar and mainContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import SignIn from "./SignIn";
+import { Slack } from ".";
+
+function Home() {
+  return <div>Home</div>;
+}
 
 class App extends Component {
-  state = {
-    counter: 1,
-  };
-
   render() {
     return (
       <div>
-        <SignIn />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/slack" component={Slack} />
+        </Switch>
       </div>
     );
   }
